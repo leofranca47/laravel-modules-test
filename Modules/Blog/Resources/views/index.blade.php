6c3bfba32d38ec99bee38db0ead49bdd5ae1ae1c
Leo França <leonardo.lightbase@sebraemg.com.br>
2022-04-08 08:37:14 -0300

    adiciona o adminlte

--- Modules/Blog/Resources/views/index.blade.php
+++ Modules/Blog/Resources/views/index.blade.php
@@ -1,9 +1,19 @@
-@extends('blog::layouts.master')
+@extends('adminlte::page')
+
+@section('title', 'Dashboard')
+
+@section('content_header')
+    <h1>Dashboard</h1>
+@stop
 
 @section('content')
-    <h1>Hello World</h1>
+    <p>estamos no modulo de blog rsrsrs</p>
+@stop
 
-    <p>
-        This view is loaded from module: {!! config('blog.name') !!}
-    </p>
-@endsection
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+
+@section('js')
+    <script> console.log('Hi!'); </script>
+@stop
--- Modules/Site/Resources/views/index.blade.php
+++ Modules/Site/Resources/views/index.blade.php
@@ -1,10 +1,19 @@
-@extends('site::layouts.master')
+@extends('adminlte::page')
+
+@section('title', 'Dashboard')
+
+@section('content_header')
+    <h1>Dashboard</h1>
+@stop
 
 @section('content')
-    <h1>Hello World</h1>
+    <p>Welcome to this beautiful admin panel.</p>
+@stop
 
-    <p>
-        This view is loaded from module: {!! config('site.name') !!}
-    </p>
-    Owner is {{$blog}}
-@endsection
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+
+@section('js')
+    <script> console.log('Hi!'); </script>
+@stop
